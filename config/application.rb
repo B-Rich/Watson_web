b8b068b5c8e619537bed5ea010c01c7d573aa302
Joanne D <codebyjo@gmail.com>
2016-04-11 23:57:23 -0400

    database for heroku

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,13 @@ module WatsonWeb3
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+
+  config.paperclip_defaults = {
+   storage: :s3,
+    s3_credentials: {
+    bucket: ENV['AWS_S3_BUCKET'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+   }
+  }
 end
